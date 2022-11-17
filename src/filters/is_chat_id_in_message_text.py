905bbe05223c4767a8e26e9befc3015112e6de6d
Eldos <eldos.baktybekov@gmail.com>
2022-11-17 11:13:54 +0600

    :bug: Fix mixed up message's text

--- src/filters/is_chat_id_in_message_text.py
+++ src/filters/is_chat_id_in_message_text.py
@@ -12,6 +12,6 @@ class IsChatIDInMessageTextFilter(BoundFilter):
 
     async def check(self, message: Message) -> bool | dict:
         try:
-            return {'from_chat_id': extract_chat_id_from_text(message.text)}
+            return {'from_chat_id': extract_chat_id_from_text(message.reply_to_message.text)}
         except exceptions.ChatIDNotFoundInMessageError:
             return False
