1b2d817555153f8188a6d3418bc423683d650f80
Eldos <eldos.baktybekov@gmail.com>
2022-11-17 10:31:31 +0600

    :sparkles: Add utility to extract message object from any kind of update

--- src/shortcuts.py
+++ src/shortcuts.py
@@ -2,6 +2,7 @@ import logging
 from typing import Iterable
 
 from aiogram import Bot
+from aiogram.types import Message, CallbackQuery, Update
 from aiogram.utils.exceptions import TelegramAPIError
 
 import exceptions
@@ -9,6 +10,7 @@ import exceptions
 __all__ = (
     'send_signed_text_message',
     'extract_chat_id_from_text',
+    'get_message',
 )
 
 
@@ -27,3 +29,15 @@ async def send_signed_text_message(bot: Bot, text: str, from_chat_id: int, to_ch
             await bot.send_message(to_chat_id, text)
         except TelegramAPIError:
             logging.warning(f'Could not send message to {to_chat_id}')
+
+
+def get_message(query: Message | CallbackQuery | Update) -> Message:
+    match query:
+        case Message():
+            return query
+        case Update() if query.callback_query is not None:
+            return query.callback_query.message
+        case CallbackQuery() | Update() if query.message is not None:
+            return query.message
+        case _:
+            raise ValueError('Query must be Message, CallbackQuery or Update type')
