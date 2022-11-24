843db62d2e21a4d02bad10e7fd2789f3c2a84314
Eldos <eldos.baktybekov@gmail.com>
2022-11-24 22:21:30 +0600

    :sparkles: Add message text length limit

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,10 +1,11 @@
 from aiogram import Dispatcher
 
-from . import admins, users
+from . import admins, users, common
 
 __all__ = ('register_handlers',)
 
 
 def register_handlers(dispatcher: Dispatcher, filters: dict):
+    common.register_handlers(dispatcher, filters)
     admins.register_handlers(dispatcher, filters)
     users.register_handlers(dispatcher)
