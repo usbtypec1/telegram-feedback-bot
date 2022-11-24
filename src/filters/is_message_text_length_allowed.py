bdc9aebf275e6e6ebd14195f39a26c43b19035f4
Eldos <eldos.baktybekov@gmail.com>
2022-11-24 21:51:37 +0600

    :bug: Fix message text limit filter
    Filter was always False if limit was set to value lower or equal to 1

--- src/filters/is_message_text_length_allowed.py
+++ src/filters/is_message_text_length_allowed.py
@@ -11,6 +11,11 @@ class IsMessageTextLengthAllowedFilter(BoundFilter):
 
     def __init__(self, text_length_limit: int):
         self.__text_length_limit = text_length_limit
+        self.__validate_text_length_limit()
+
+    def __validate_text_length_limit(self):
+        if self.__text_length_limit <= 1:
+            raise ValueError('Length limit must be greater than 1')
 
     async def check(self, message: Message) -> bool:
         return len(message.text) < self.__text_length_limit
