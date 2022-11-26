5d916e183fafc57b7a6cf5bfc6b3a273f062493c
Eldos <eldos.baktybekov@gmail.com>
2022-11-26 22:56:51 +0600

    :recycle: Refactor message text length filter
    * Add optional parameter `is_message_text_required`.

--- src/filters/is_message_text_length_allowed.py
+++ src/filters/is_message_text_length_allowed.py
@@ -7,10 +7,11 @@ __all__ = ('IsMessageTextLengthAllowedFilter',)
 class IsMessageTextLengthAllowedFilter(BoundFilter):
     key = 'message_text_limit'
 
-    __slots__ = ('text_length_limit',)
+    __slots__ = ('text_length_limit', '__is_message_text_required')
 
-    def __init__(self, text_length_limit: int):
+    def __init__(self, text_length_limit: int, *, is_message_text_required: bool = False):
         self.__text_length_limit = text_length_limit
+        self.__is_message_text_required = is_message_text_required
         self.__validate_text_length_limit()
 
     def __validate_text_length_limit(self):
@@ -18,5 +19,7 @@ class IsMessageTextLengthAllowedFilter(BoundFilter):
             raise ValueError('Length limit must be greater than 1')
 
     async def check(self, message: Message) -> bool:
-        message_text = message.text or message.caption or ''
+        message_text = message.text or message.caption
+        if message_text is None:
+            return not self.__is_message_text_required
         return len(message_text) < self.__text_length_limit
